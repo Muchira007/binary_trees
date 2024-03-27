8a7045d93106fdb6ed5649009518439df691dae8
muchira007 <muchirasteve2@gmail.com>
2024-03-27 13:42:56 -0700

    compiled the two files

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,7 +9,7 @@
  *	Otherwise value
  */
 
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *mzae;
 
